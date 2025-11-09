39b7e110dc28f994612f1ca2013176660574c7a6
vanichols <virginia.nichols@gmail.com>
2025-11-09 09:58:36 +0100

    successfully load adoptpkg

--- app.R
+++ app.R
@@ -1,51 +1,50 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+# app.R
+library(tidyverse)
+library(readxl)
+# Gina Nichols, adapted from 
+# Noé Vandevoorde, October 2025
 
-library(shiny)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+#### Packages ##################################################################
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+load_packages <- function(pkgs) {
+  for (pkg in pkgs) {
+    if (!require(
+        pkg,
+        character.only = TRUE,
+        quietly = TRUE,
+        warn.conflicts = FALSE)) {
+      install.packages(
+        pkg,
+        dependencies = TRUE)
+      library(
+        pkg,
+        character.only = TRUE,
+        warn.conflicts = FALSE)
+    }
+  }
 }
 
-# Run the application 
+load_packages(c(
+   "tidyverse",
+   "shiny",
+   "shinydashboard",
+   "readxl",
+   "ggnewscale",
+   "ggpattern",
+   "devtools"
+))
+
+devtools::install_github("vanichols/ADOPTpkg")
+library(ADOPTpkg)
+
+#### Sources ###################################################################
+
+source("R/prep_utils.R")
+source("R/ui.R")
+source("R/server.R")
+
+
+#### Run the app ###############################################################
+
 shinyApp(ui = ui, server = server)
